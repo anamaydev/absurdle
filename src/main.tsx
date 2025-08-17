cc3147730250afcd7209de03fa7610a64e720ed4
anamaydev <anamaydev@gmail.com>
2025-08-17 13:44:20 +0530

    chore: add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
-import App from './App.tsx'
+import Login from "./pages/Login.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login/>
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
