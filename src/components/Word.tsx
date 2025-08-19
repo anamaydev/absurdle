5204735da41d4e32d0cf391d4b22402556358348
anamaydev <anamaydev@gmail.com>
2025-08-19 12:51:46 +0530

    feat: add word reveal animation

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -5,10 +5,11 @@ type WordProps = {
   result: number[],
   isCellBlocked: boolean,
   shakeIt: boolean,
+  reveal: boolean,
   onShakeEnd: () => void,
 }
 
-const Word = ({word, result, isCellBlocked, shakeIt, onShakeEnd}: WordProps) => {
+const Word = ({word, result, isCellBlocked, shakeIt, reveal, onShakeEnd}: WordProps) => {
   return (
     <div
       // className="flex gap-1.5 sm:gap-2"
@@ -16,12 +17,28 @@ const Word = ({word, result, isCellBlocked, shakeIt, onShakeEnd}: WordProps) => 
       onAnimationEnd={onShakeEnd} /* reset after animation */
     >
       {word.split("").map((letter: string, index: number) => (
+        <div
+          key={index}
+          className={clsx("relative size-13 sm:size-15 font-roboto text-2xl transition-transform transform-3d duration-600", reveal && "rotate-x-180")}
+          style={{transitionDelay: `${index * 150}ms`}}
+        >
+          {/* front */}
+          <div className="absolute inset-0 z-20 flex justify-center items-center border-2 rounded-lg bg-black-900 border-grey-800 rotate-x-0 backface-hidden">
+            {letter.toUpperCase()}
+          </div>
+
+          {/* back */}
           <div
-            key={index}
-            className="flex justify-center items-center size-13 sm:size-15 rounded-lg border-2 border-grey-800 font-roboto text-2xl"
+            className={clsx( "absolute z-10 inset-0 flex justify-center " +
+              "items-center border-2 rounded-lg rotate-x-180 backface-hidden",
+              result[index] === 1 && "bg-green-600 border-green-600",
+              result[index] === 2 && "bg-yellow-600 border-yellow-600",
+              result[index] === -1 && "bg-grey-800 border-grey-800",
+            )}
           >
             {letter.toUpperCase()}
           </div>
+        </div>
       ))}
     </div>
   )
